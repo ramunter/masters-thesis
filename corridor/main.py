4900b3f2aa4bfe37e5bbc0d76483dd5e0420e67d
Unknown <rasmusmunter@gmail.com>
2019-01-28 17:29:50 +0100

    Add flags

--- corridor/main.py
+++ corridor/main.py
@@ -1,12 +1,22 @@
-from experiment import increasing_chain_length_experiment
+from absl import flags
+
 from numpy import arange
+
+from experiment import increasing_chain_length_experiment
 
 from regular_q_learner import q_learner
 from ube_q_learner import ube_q_learner, sample_target_ube_q_learner
 
+
+flags.DEFINE_integer("iterations", 5, "Number of attempts per chain length")
+flags.DEFINE_integer("longest_chain", 10, "Longest chain attempted")
+
+FLAGS = flags.FLAGS 
+
+
 increasing_chain_length_experiment(
     [q_learner, ube_q_learner, sample_target_ube_q_learner],
     ["Regular", "UBE", "UBE with sample target"],
-    chain_length_sequence=arange(4, 20, 2),
-    attempts_per_chain_length=5)
+    chain_length_sequence=arange(2, FLAGS.longest_chain, 2),
+    attempts_per_chain_length=FLAGS.iterations)
 
